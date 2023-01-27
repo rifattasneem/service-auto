ce601be6704fc84aa0251a2d28f620f76a1a3da3
rifattasneem <tasneem0795@gmail.com>
2023-01-27 22:30:54 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
+// Navbar toggler
 const navbarToggler = document.querySelector(".navbar-toggler");
 const navbarMenu = document.querySelector(".navbar ul");
 const navbarLinks = document.querySelectorAll(".navbar a");
 
 navbarToggler.addEventListener("click", navbarTogglerClick);
-
 function navbarTogglerClick() {
   navbarToggler.classList.toggle("open-navbar-toggler");
   navbarMenu.classList.toggle("open");
 }
 
 navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
-
 function navbarLinkClick() {
   if(navbarMenu.classList.contains("open")) {
     navbarToggler.click();
   }
 }
 
+// Navbar background scroll blur
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("header").classList.add('blur');
